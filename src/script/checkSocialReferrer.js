d83ba5c167c07cdf3e10363f495bf708df6b4942
vhsr9479 <victor.salazar.ramirez.11.1@gmail.com>
2025-08-19 12:45:50 -0500

    Plantilla Contruproyectovm Finalizada en la primera entrega

--- src/script/checkSocialReferrer.js
+++ src/script/checkSocialReferrer.js
@@ -2,16 +2,12 @@ export function checkSocialReferrer() {
   const ref = document.referrer.toLowerCase();
 
   if (ref.includes("facebook.com")) {
-    console.log("El usuario viene desde Facebook");
     return "facebook";
   } else if (ref.includes("instagram.com")) {
-    console.log("El usuario viene desde Instagram");
     return "instagram";
   } else if (ref.includes("tiktok.com")) {
-    console.log("El usuario viene desde TikTok");
     return "tiktok";
   } else {
-    console.log("El usuario no viene de Facebook, Instagram ni TikTok");
     return null;
   }
 }
